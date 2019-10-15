2bcd5a0439f646669a52123fc1f96329ecb6dad6
syphan <phantiensy@unimarmores.com>
2019-10-15 17:54:20 +0700

    refactor code

--- src/main/java/com/syphan/practice/registrationservice/service/impl/PermissionServiceImpl.java
+++ src/main/java/com/syphan/practice/registrationservice/service/impl/PermissionServiceImpl.java
@@ -1,8 +1,8 @@
 package com.syphan.practice.registrationservice.service.impl;
 
-import com.syphan.practice.commonservice.dao.JpaQueryRepository;
 import com.syphan.practice.commonservice.service.impl.BaseServiceImpl;
 import com.syphan.practice.registrationservice.model.Permission;
+import com.syphan.practice.registrationservice.repository.PermissionRepository;
 import com.syphan.practice.registrationservice.service.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class PermissionServiceImpl extends BaseServiceImpl<Permission, Integer> implements PermissionService {
 
     @Autowired
-    protected PermissionServiceImpl(JpaQueryRepository<Permission, Integer> repository) {
+    protected PermissionServiceImpl(PermissionRepository repository) {
         super(repository);
     }
 }
