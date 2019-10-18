567fe2d5176baecd0810599d4ea8d3341bdfbf94
syphan <phantiensy@unimarmores.com>
2019-10-18 08:59:25 +0700

    impl create role and permission

--- src/main/java/com/syphan/practice/registrationservice/service/impl/RoleServiceImpl.java
+++ src/main/java/com/syphan/practice/registrationservice/service/impl/RoleServiceImpl.java
@@ -12,9 +12,7 @@ import com.syphan.practice.registrationservice.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Service
